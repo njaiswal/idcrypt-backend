b66f91e5a6c4f33e03c38ef5c067984555cc502b
njaiswal <Nishant Jaiswal>
2020-02-08 23:30:33 +0000

    minor update to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 npm install -g serverless
+serverless plugin install -n serverless-python-requirements
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r eu-west-1
