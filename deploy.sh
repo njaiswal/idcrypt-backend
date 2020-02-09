a3605c28cb419f003199530389216fa82851f084
njaiswal <Nishant Jaiswal>
2020-02-09 10:13:52 +0000

    moving build into .serverless dir

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
 npm install -g serverless
+
+# Due to a bug (https://github.com/serverless/serverless/issues/5381) we have to move build into .serverless dir
+mkdir -p .serverless
+cp -r $CODEBUILD_SRC_DIR/target/$env/* $PWD/.serverless
 serverless plugin install -n serverless-python-requirements
-serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r eu-west-1
+serverless deploy --stage $env --package .serverless -v -r eu-west-1
