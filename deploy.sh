2ee0ad8d365b6f0cc50e24430540969f5f469c30
njaiswal <Nishant Jaiswal>
2020-02-10 09:28:47 +0000

    Adding dev.api.idcrypt.io domain to deployment.

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ npm install -g serverless
 mkdir -p .serverless
 cp -r $CODEBUILD_SRC_DIR/target/$env/* $PWD/.serverless
 serverless plugin install -n serverless-python-requirements
+npm install serverless-domain-manager@3.3.1 --save-dev
 serverless deploy --stage $env --package .serverless -v -r eu-west-1
